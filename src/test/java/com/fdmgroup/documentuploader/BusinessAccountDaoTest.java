afab13ed718b0d9d0e1cf26ae5803d0c25e9754e
Leslie Vicente <leslie.vicente@fdmgroup.com>
2018-09-06 14:14:57 -0400

    added new business acount dao class and added sql procedure to generate id for business account

--- src/test/java/com/fdmgroup/documentuploader/BusinessAccountDaoTest.java
+++ src/test/java/com/fdmgroup/documentuploader/BusinessAccountDaoTest.java
@@ -1,5 +1,14 @@
 package com.fdmgroup.documentuploader;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class BusinessAccountDaoTest {
+
+	@Test
+	public void test_willCheckIfBusinessAccountCreated() {
+		fail("Not yet implemented");
+	}
 
 }
