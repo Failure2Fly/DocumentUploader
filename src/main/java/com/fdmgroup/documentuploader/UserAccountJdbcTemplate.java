4fcc72252af15f830d9dda9ed43b018b61bc64b1
Daniel Klingensmith <daniel.klingensmith@fdmgroup.com>
2018-09-06 09:29:02 -0400

    Debugging dispatcher to useraccount create

--- src/main/java/com/fdmgroup/documentuploader/UserAccountJdbcTemplate.java
+++ src/main/java/com/fdmgroup/documentuploader/UserAccountJdbcTemplate.java
@@ -1,6 +1,9 @@
 package com.fdmgroup.documentuploader;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map.Entry;
 
 import javax.sql.DataSource;
@@ -20,11 +23,22 @@ public class UserAccountJdbcTemplate implements DAO<UserAccount,String> {
 
    	String SQL1="INSERT INTO USERACCOUNT (userid,username,lastname,firstname,userpassword,useremail) VALUES(useraccount_seq.nextval,?,?,?,?,?)";
    	//String SQL2="INSERT INTO USERACCOUNTTOSECURITYQUESTION VALUES(useraccount_seq.currval,?,?)";
-   	Entry<SecurityQuestion, String> entry = item.getMapQA().entrySet().iterator().next();
+//   	Entry<SecurityQuestion, String> entry = item.getMapQA().entrySet().iterator().next();
 
-   	String key = entry.getKey().name().toLowerCase().replace("_", " ")+"?";
-   	key = key.substring(0, 1).toUpperCase() + key.substring(1);
-    jdbcTemplateObject.update(SQL1,item.getUsername(),item.getLastName(),item.getFirstName(),item.getPassword(),item.getEmailAddress());
+//   	String key = entry.getKey().name().toLowerCase().replace("_", " ")+"?";
+//   	key = key.substring(0, 1).toUpperCase() + key.substring(1);
+   	File file = new File("H:\\DebugInCreate.txt");
+	try {
+		FileWriter writer= new FileWriter(file);
+		writer.write(item.toString()); 
+	      writer.flush();
+	      writer.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+    jdbcTemplateObject.update(SQL1,item.getUsername(),item.getLastName(),item.getFirstName(),item.getPassword(),item.getUserEmail());
     //jdbcTemplateObject.update(SQL2, entry.getKey().ordinal()+1,entry.getValue());
 
 
@@ -43,12 +57,12 @@ public class UserAccountJdbcTemplate implements DAO<UserAccount,String> {
    public void update(UserAccount item) {
    	//TODO sql query update username
    	String SQL = "UPDATE useraccount SET firstname=?,lastname=?,userpassword=?,useremail=? WHERE username=?";
-   	jdbcTemplateObject.update(SQL,item.getFirstName(),item.getLastName(),item.getPassword(),item.getEmailAddress(),item.getUsername());
+   	jdbcTemplateObject.update(SQL,item.getFirstName(),item.getLastName(),item.getPassword(),item.getUserEmail(),item.getUsername());
    }
    
    @Override
    public UserAccount read(String username) {
-	  String SQL = "SELECT use.username,use.userpassword,use.useremail,use.firstname,use.lastname,sec.question,uats.questionanswer FROM USERACCOUNT USE JOIN USERACCOUNTTOSECURITYQUESTION UATS ON UATS.USERACCOUNTSECURITYJOINID = USE.USERID JOIN SECURITYQUESTION SEC ON UATS.SECURITYQUESTIONJOINID = SEC.QUESTIONID WHERE USERNAME = ?";
+	  String SQL = "SELECT userid,username,lastname,firstname,userpassword,useremail FROM UserAccount WHERE Username = ?";
 	  UserAccount user = jdbcTemplateObject.queryForObject(SQL,new Object[]{username},new UserAccountMapper());
    	return user;
    }
