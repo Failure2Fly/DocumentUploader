8087875eb3896476dcf98cc07a7935cf5f42388d
Yenaineh Mengistu <yenaineh.mengistu@fdmgroup.com>
2018-09-11 09:53:19 -0400

    context.xml changed useraccount bean class and id to useraccountdao

--- src/main/java/com/fdmgroup/documentuploader/DispatchController.java
+++ src/main/java/com/fdmgroup/documentuploader/DispatchController.java
@@ -200,7 +200,7 @@ public class DispatchController {
 
 	}
 
-	@RequestMapping(value = "/accountDetails/{accountId}", method = RequestMethod.GET)
+	@RequestMapping(value = "/account/{accountId}", method = RequestMethod.GET)
 	public String AccountDetailsGet(Model model, HttpSession session,
 			@PathVariable(value = "accountId") String accountId) {
 		BusinessAccountDao businessDao = (BusinessAccountDao) context.getBean("BusinessAccountDao");
@@ -212,7 +212,7 @@ public class DispatchController {
 
 	}
 
-	@RequestMapping(value = "/accountDetails/{accountId}", method = RequestMethod.POST)
+	@RequestMapping(value = "/account/{accountId}", method = RequestMethod.POST)
 	public String AccountDetailsPost(@ModelAttribute Document document, HttpSession session, @PathVariable(value = "accountId") String accountId,@RequestParam("file") MultipartFile file) {
 		DocumentDao documentDao = (DocumentDao) context.getBean("DocumentDao");
 		File directory = new File("H:\\repository\\"+accountId);
