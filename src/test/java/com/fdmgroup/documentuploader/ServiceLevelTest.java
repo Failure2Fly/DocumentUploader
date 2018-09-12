286806a9815d4a7bc8f27cae7862b1ebe2b93181
Brandon Porter <brandon.porter@fdmgroup.com>
2018-09-12 10:45:09 -0400

    Updated ServiceLevelTest

--- src/test/java/com/fdmgroup/documentuploader/ServiceLevelTest.java
+++ src/test/java/com/fdmgroup/documentuploader/ServiceLevelTest.java
@@ -37,7 +37,64 @@ public class ServiceLevelTest {
 		// Arrange
 		
 		// Act
-		SerivceLevel result = new ServiceLevel(ServiceLevels.BRONZE);
+		ServiceLevel result = new ServiceLevel(ServiceLevels.BRONZE);
+		
+		// Assert
+		assertEquals(result.getServiceLevel(), ServiceLevels.BRONZE);
+		assertEquals(result.getDocumentUploadLimit(), 2);
+		assertEquals(result.getHasAdverts(), true);
+	}
+	
+	@Test
+	public void test_constructor_CreaturesServiceLevelOfSilver_whenProvidedWithSilverLevel() {
+		// Arrange
+		
+		// Act
+		ServiceLevel result = new ServiceLevel(ServiceLevels.SILVER);
+		
+		// Assert
+		assertEquals(result.getServiceLevel(), ServiceLevels.SILVER);
+		assertEquals(result.getDocumentUploadLimit(), 5);
+		assertEquals(result.getHasAdverts(), true);
+	}
+	
+	@Test
+	public void test_constructor_CreaturesServiceLevelOfGold_whenProvidedWithGoldLevel() {
+		// Arrange
+		
+		// Act
+		ServiceLevel result = new ServiceLevel(ServiceLevels.GOLD);
+		
+		// Assert
+		assertEquals(result.getServiceLevel(), ServiceLevels.GOLD);
+		assertEquals(result.getDocumentUploadLimit(), 20);
+		assertEquals(result.getHasAdverts(), false);
+	}
+	
+	@Test
+	public void test_constructor_CreaturesServiceLevelOfUnlimited_whenProvidedWithUnlimitedLevel() {
+		// Arrange
+		
+		// Act
+		ServiceLevel result = new ServiceLevel(ServiceLevels.UNLIMITED);
+		
+		// Assert
+		assertEquals(result.getServiceLevel(), ServiceLevels.UNLIMITED);
+		assertEquals(result.getDocumentUploadLimit(), -1);
+		assertEquals(result.getHasAdverts(), false);
+	}
+	
+	@Test
+	public void test_constructor_CreaturesServiceLevelOfEnterprise_whenProvidedWithEnterpriseLevel() {
+		// Arrange
+		
+		// Act
+		ServiceLevel result = new ServiceLevel(ServiceLevels.ENTERPRISE);
+		
+		// Assert
+		assertEquals(result.getServiceLevel(), ServiceLevels.ENTERPRISE);
+		assertEquals(result.getDocumentUploadLimit(), -1);
+		assertEquals(result.getHasAdverts(), false);
 	}
 
 }
